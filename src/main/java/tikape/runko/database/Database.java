53dbad97e3244614945714ce3438cd6cec0e2438
Olli <ollikarki94@gmail.com>
2017-10-19 15:09:35 +0300

    Ollin mestariteos

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -40,9 +40,20 @@ public class Database {
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
         lista.add("CREATE TABLE Annos (id integer PRIMARY KEY, nimi text);");
+        lista.add("INSERT INTO Annos VALUES(0, 'Mega Smoothie');");
+        lista.add("INSERT INTO Annos VALUES(1, 'Super Smoothie');");
+        lista.add("INSERT INTO Annos VALUES(2, 'Ultra Smoothie');");
         lista.add("CREATE TABLE RaakaAine (id integer PRIMARY KEY, nimi text);");
+        lista.add("INSERT INTO RaakaAine VALUES(0, 'Maito');");
+        lista.add("INSERT INTO RaakaAine VALUES(1, 'Vesi');");
+        lista.add("INSERT INTO RaakaAine VALUES(2, 'Hiiva');");
         lista.add("CREATE TABLE AnnosRaakaAine (raakaAine_id INTEGER, annos_id INTEGER, jarjestys INTEGER, maara INTEGER, ohje TEXT, FOREIGN KEY (raakaAine_id) References RaakaAine(id), FOREIGN KEY (annos_id) REFERENCES Annos(id));");
-
+        lista.add("INSERT INTO AnnosRaakaAine VALUES(0, 0, 1, 1, 'Kauniisti');");
+        lista.add("INSERT INTO AnnosRaakaAine VALUES(0, 1, 1, 1, 'Rumasti');");
+        lista.add("INSERT INTO AnnosRaakaAine VALUES(0, 2, 1, 1, 'Jotenkin');");
+        lista.add("INSERT INTO AnnosRaakaAine VALUES(1, 0, 2, 1, 'Vaikka väkisin');");
+        lista.add("INSERT INTO AnnosRaakaAine VALUES(2, 1, 2, 1, 'Tunget vaan sinne');");
+        
         return lista;
     }
 }
