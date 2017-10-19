6ab2ca52adc2ae10cf332c9cc21730ada73d4da1
unknown <samuli.ahlqvist@helsinki.fi>
2017-10-19 14:26:19 +0300

    index muutoksii

--- src/main/java/tikape/runko/database/RaakaaineDao.java
+++ src/main/java/tikape/runko/database/RaakaaineDao.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.domain.Raakaaine;
-import tikape.runko.domain.Smoothie;
 
 public class RaakaaineDao implements Dao<Raakaaine, Integer> {
 
